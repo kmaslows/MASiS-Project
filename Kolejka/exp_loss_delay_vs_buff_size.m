8af08ae63187c34159319839e6c822c8babae670
Kamil Masłowski <kmaslows@student.agh.edu.pl>
2016-05-11 18:37:20 +0200

    Experiment example
    Packet lost vs buffer size

--- Kolejka/exp_loss_delay_vs_buff_size.m
+++ Kolejka/exp_loss_delay_vs_buff_size.m
@@ -2,9 +2,9 @@ close all; clear all;
 addpath(genpath('../Raw-data'));
 
 % Set buffor sizes
-B = 50;
+B = 10:20;
 % Set queue throughput
-C = [0.5*10^7]; % [B/s]
+C = [1*10^7]; % [B/s]
 
 % Load and prepare data
 data = load('ispdsl.mat');
@@ -13,4 +13,7 @@ A = A(A(:,3)==1, :);
 %A = A(1:1000,:);
 
 output = queue_batch_wrapper( A, B, C );
-save('test.mat','output');
+x = cell2mat(output(:,1));
+y = cell2mat(output(:,3));
+plot(x,y);
+
