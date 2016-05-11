5400d788d49245f4af6345e65cd93829841902c5
Kamil Masłowski <kmaslows@student.agh.edu.pl>
2016-05-11 21:58:39 +0200

    Small performance fix

--- Kolejka/queue_batch_wrapper.m
+++ Kolejka/queue_batch_wrapper.m
@@ -15,22 +15,20 @@ b = b(:);
 c = c(:);
 output = num2cell([b c]);
 %output{1,3} = [];
-output_lost = {};
-output_lostvec = {};
-output_Xvec = {};
+output_lost = cell(n,1);
+output_lostvec = cell(n,1);
+output_Xvec = cell(n,1);
 %% Run
-parfor i = 1:n,
+parfor (i = 1:n),
     tic
-    [ lost lostvec Xvec ] = queue( A, b(i), c(i) );
+    [ output_lost{i}, output_lostvec{i}, output_Xvec{i} ] = queue( A, b(i), c(i) );
     disp('-----------------------------------')
-    disp(['Run = ', num2str(i) , '/', num2str(n), ' B = ', num2str(b(i)), ' C = ', num2str(c(i))]);
-    disp(['Packet lost: ', num2str(lost)]);
+    disp(['Run = ', num2str(i) , '/', num2str(n), ' B = ', num2str(b(i)), ' C = ', num2str(c(i))])
+    disp(['Packet lost: ', num2str(output_lost{i})])
     toc
-    output_lost(i) = {lost};
-    output_lostvec(i) =  {lostvec};
-    output_Xvec(i) = {Xvec};
 end
 
-output = [output output_lost' output_lostvec' output_Xvec'];
+
+output = [output output_lost output_lostvec output_Xvec];
 
 end
